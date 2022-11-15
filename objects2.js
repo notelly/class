f004e77a845228d7847b1b5cba5e9dd0973f4bb3
notelly <117340086+notelly@users.noreply.github.com>
2022-11-15 17:44:15 +0900

    Update objects2.js

--- objects2.js
+++ objects2.js
@@ -8,11 +8,14 @@ class Book{
         this.press = press;
         this.price = price;
     }
+    showAuthor(){
+        return `${this.title}의 저자는 ${this.author} 입니다.`
+    }
 }
 // 다산북스, 김영사, 범우사
 let b1 =  new Book('오만과편견', '제인오스틴', '민음사', 1.5);
 let b2 =  new Book('데미안', '헤르만 헤세', '민음사', 1.0);
-let b3 =  new Book('헝거게임', '미상', '김영사', 1.3);
+let b3 =  new Book('헝거게임', '에이미', '김영사', 1.3);
 let b4 =  new Book('해리포터', 'JK.롤링', '김영사', 1.4);
 let b5 =  new Book('국어사전', '우리만연구소', '범우사', 1.7);
 let b6 =  new Book('로미오와 줄리엣', '세익스피어', '범우사', 2.0);
@@ -28,7 +31,13 @@ books.push(b6);
 
 
 function findBooks(press) {
-
+    let prMatch = [];
+        for(let bk of books){
+            if(bk.press == press){
+                prMatch.push(bk);
+            }
+        }
+    return prMatch;
 }
 
 let result = findBooks("김영사"); //출판사 정보가 같은 것 출력하도록
